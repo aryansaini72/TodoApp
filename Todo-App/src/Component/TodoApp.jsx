981369a9fc040538bf7f4389ce36e6995b605496
aryansaini72 <sainidhorke8@gmail.com>
2025-02-07 16:08:53 +0530

    add file

---
+++ Todo-App/src/Component/TodoApp.jsx
@@ -0,0 +1,85 @@
+import React, { useState } from "react";
+
+const TodoApp = () => {
+  const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState("");
+  const [edit, setEdit] = useState(false);
+  const [editIndex, setEditIndex] = useState(null);
+
+  function Todo(e) {
+    e.preventDefault();
+    if (input.trim()) {
+      setTodos([...todos, input]);
+      setInput("");
+    }
+  }
+
+  const startEditTodo = (index) => {
+    setEdit(true);
+    setEditIndex(index);
+    setInput(todos[index]);
+  };
+  const saveEditTodo = () => {
+    if (input && editIndex !== null) {
+      let updateTodo = todos.map((todo, i) => (i === editIndex ? input : todo));
+      setTodos(updateTodo);
+      setEdit(false);
+      setEditIndex(null);
+      setInput("");
+    }
+  };
+
+  const deleteTodo = (index) => {
+    if (index !== null) {
+      let delTodo = todos.filter((_, i) => i !== index)
+      setTodos(delTodo);
+    }
+  }
+
+  return (
+    <div className="main-container">
+      <div className="center-container">
+        <h1 className="app-heading">Todo App</h1>
+        <div className="input-container">
+          <input
+            type="text"
+            value={input}
+            placeholder="Add a New Task"
+            onChange={(e) => setInput(e.target.value)}
+            className="input-box-todo"
+          />
+          {edit ? (
+            <button onClick={saveEditTodo} className="add-btn">
+              Save
+            </button>
+          ) : (
+            <button onClick={Todo} className="add-btn">
+              Add
+            </button>
+          )}
+        </div>
+
+        <div>
+          <h1 className="app-heading"> Todo List</h1> <hr />
+          {todos.map((task, index) => (
+            <ul key={index} className="list-item">
+              <li>{task}</li>
+              <li className="btn-click">
+                <i
+                  onClick={() => startEditTodo(index)}
+                  class="fa-solid fa-pen-to-square"
+                ></i>
+                <i
+                  onClick={() => deleteTodo(index)}
+                  className="fa-solid fa-trash-can icon-delete"
+                ></i>
+              </li>
+            </ul>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TodoApp;
